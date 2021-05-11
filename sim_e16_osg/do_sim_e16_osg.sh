3395de652d46144c2c21ad52a8557fd9fd763f22
Chris_McLauchlin_Laptop2 <c.a.mclauchlin@gmail.com>
2021-05-11 10:59:38 -0400

    added the cleanup portion
    cleaning up some stuff

--- sim_e16_osg/do_sim_e16_osg.sh
+++ sim_e16_osg/do_sim_e16_osg.sh
@@ -83,7 +83,7 @@ echoerr "============ end h10maker ============"
 echoerr "============ cleanup ============"
 du -sh *
 echoerr "============ cleanup ============"
-#rm -rf parms parms.tar.gz gpp.out gpp.out.tmp gsim.out gsim_e16.inp twopi_e16.inp user_ana_e16.tcl
+rm -rf ana.hbook ana.out gpp.hbook gpp.out gpp.out.tmp gsim.out gsim_e16.inp out.obs out_hist_test.root parms parms.tar.gz twopi_e16.inp user_ana_e16.tcl
 echoerr "============ cleanup ============"
 du -sh *
 echoerr "============ cleanup ============"
